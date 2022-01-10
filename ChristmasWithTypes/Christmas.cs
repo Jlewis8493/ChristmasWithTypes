f5ef81c3b2e3924b7491777e500bc9fe50163735
Jared Lewis <jlewis8493@gmail.com>
2022-01-10 01:26:38 -0500

    Completed tree and Day properties

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -4,10 +4,7 @@ namespace ChristmasWithTypes
     public class Christmas
     {
         public string[] Presents;
-        public string[] Cars;
-        public string[] Boats;
-        public string[] Planes;
-        public string KrisKringle { get; set; }
+        public string Santa { get; set; }
         public int? TreeHeight { get; set; } //DONE Make the Height Property Nullable
 
         //DONE Make the property, "Day", type enum
